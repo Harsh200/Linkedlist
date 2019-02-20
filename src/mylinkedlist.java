bba1c663afb25ee42149e2066755f9f5f686b4ac
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-20 11:30:35 +0530

    new class MyLinked list

---
+++ src/mylinkedlist.java
@@ -0,0 +1,35 @@
+public class mylinkedlist {
+    private node head;
+    private node tail;
+    private int currentSize;
+
+    public node getHead() {
+        return head;
+    }
+
+    public void setHead(node head) {
+        this.head = head;
+    }
+
+    public node getTail() {
+        return tail;
+    }
+
+    public void setTail(node tail) {
+        this.tail = tail;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
+
+    public mylinkedlist() {
+        this.head=null;
+        this.tail=null;
+        this.currentSize=0;
+    }
+}
