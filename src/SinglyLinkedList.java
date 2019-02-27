752c9bcba4e9cd911ffeb254897ae2fdab79622e
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-02-27 10:40:41 +0530

    this is a doubly linkedlist program

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,137 +1,137 @@
-public class SinglyLinkedList {
-    private class Node {
-        private int data;
-        private Node next;
-
-        public int getData() {
-            return data;
-        }
-
-        public void setData(int data) {
-            this.data = data;
-        }
-
-        public Node(int data) {
-            this.data = data;
-        }
-
-        public Node(int data, Node next) {
-            this.data = data;
-            this.next = next;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-    }
-
-    private Node head = null;
-    private int size = 0;
-//this is main method
-    public static void main(String[] args) {
-        SinglyLinkedList LinkedList = new SinglyLinkedList();
-        System.out.println(LinkedList);
-        LinkedList.insert(42);
-        LinkedList.insert(43);
-        for (int i = 0; i < 5; i++) {
-            LinkedList.insertHead(i + 1);
-
-        }
-        LinkedList.insertHead(42);
-        System.out.println(LinkedList);
-    }
-    //end of main method
-
-    @Override
-    public String toString() {
-        StringBuilder response = new StringBuilder();
-        response.append("[");
-        Node temp = this.head;
-
-
-        while (temp != null) {
-            response.append(temp.getData());
-            if (temp.next != null) {
-                response.append(" ==> ");
-            }
-
-            temp = temp.next;
-            //}
-        }
-        response.append("]");
-        return response.toString();
-    }
-//insert head method
-    private void insertHead(int data) {
-//    Node newNode=new Node(data,this.head);
-//    this.head=newNode;
-        this.head = new Node(data, this.head);
-        size++;
-
-    }
-//insert after method
-    private void insertAfter(int data, Node node) {
-        Node newNode = new Node(data, node.next);
-        node.next = newNode;
-        size++;
-
-
-    }
-    //insert method
-
-    public void insert(int data) {
-        if (head == null) {
-            insertHead(data);
-        } else {
-            Node temp = this.head;
-            while ((temp.next != null)) {
-                temp = temp.next;
-            }
-            insertAfter(data, temp);
-        }
-
-    }
-
-    private int removeHead() {
-        int response = -1;
-        Node temp = this.head;
-        if (temp != null) {
-            this.size--;
-            response = temp.data;
-            this.head = this.head.next;
-        }
-        return response;
-    }
-
-public int remove(int data){
-int response=-1;
-Node temp=this.head;
-if(temp.data==data){
-response=removeAfter(temp);
-}
-else{
-    while(temp.data==data){
-        response=removeAfter(temp);
-        break;
-    }
-}
-return response;
-}
-
-    private int removeAfter(Node node) {
-        int response = -1;
-        Node temp=node.next;
-        if(temp!=null) {
-            response = temp.data;
-            node.next = temp.next;
-            size--;
-        }
-        return response;
-
-    }
-}
+//public class SinglyLinkedList {
+//    private class Node {
+//        private int data;
+//        private Node next;
+//
+//        public int getData() {
+//            return data;
+//        }
+//
+//        public void setData(int data) {
+//            this.data = data;
+//        }
+//
+//        public Node(int data) {
+//            this.data = data;
+//        }
+//
+//        public Node(int data, Node next) {
+//            this.data = data;
+//            this.next = next;
+//        }
+//
+//        public Node getNext() {
+//            return next;
+//        }
+//
+//        public void setNext(Node next) {
+//            this.next = next;
+//        }
+//
+//    }
+//
+//    private Node head = null;
+//    private int size = 0;
+////this is main method
+//    public static void main(String[] args) {
+//        SinglyLinkedList LinkedList = new SinglyLinkedList();
+//        System.out.println(LinkedList);
+//        LinkedList.insert(42);
+//        LinkedList.insert(43);
+//        for (int i = 0; i < 5; i++) {
+//            LinkedList.insertHead(i + 1);
+//
+//        }
+//        LinkedList.insertHead(42);
+//        System.out.println(LinkedList);
+//    }
+//    //end of main method
+//
+//    @Override
+//    public String toString() {
+//        StringBuilder response = new StringBuilder();
+//        response.append("[");
+//        Node temp = this.head;
+//
+//
+//        while (temp != null) {
+//            response.append(temp.getData());
+//            if (temp.next != null) {
+//                response.append(" ==> ");
+//            }
+//
+//            temp = temp.next;
+//            //}
+//        }
+//        response.append("]");
+//        return response.toString();
+//    }
+////insert head method
+//    private void insertHead(int data) {
+////    Node newNode=new Node(data,this.head);
+////    this.head=newNode;
+//        this.head = new Node(data, this.head);
+//        size++;
+//
+//    }
+////insert after method
+//    private void insertAfter(int data, Node node) {
+//        Node newNode = new Node(data, node.next);
+//        node.next = newNode;
+//        size++;
+//
+//
+//    }
+//    //insert method
+//
+//    public void insert(int data) {
+//        if (head == null) {
+//            insertHead(data);
+//        } else {
+//            Node temp = this.head;
+//            while ((temp.next != null)) {
+//                temp = temp.next;
+//            }
+//            insertAfter(data, temp);
+//        }
+//
+//    }
+//
+//    private int removeHead() {
+//        int response = -1;
+//        Node temp = this.head;
+//        if (temp != null) {
+//            this.size--;
+//            response = temp.data;
+//            this.head = this.head.next;
+//        }
+//        return response;
+//    }
+//
+//public int remove(int data){
+//int response=-1;
+//Node temp=this.head;
+//if(temp.data==data){
+//response=removeAfter(temp);
+//}
+//else{
+//    while(temp.data==data){
+//        response=removeAfter(temp);
+//        break;
+//    }
+//}
+//return response;
+//}
+//
+//    private int removeAfter(Node node) {
+//        int response = -1;
+//        Node temp=node.next;
+//        if(temp!=null) {
+//            response = temp.data;
+//            node.next = temp.next;
+//            size--;
+//        }
+//        return response;
+//
+//    }
+//}
