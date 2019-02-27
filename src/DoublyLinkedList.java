5823a569c7686b5394e711fd90d87f39fdf11ee3
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-02-27 10:42:13 +0530

    creating constructor and settled value

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -3,5 +3,11 @@ private static class Node{
 private int data;
 private Node pre;
 private Node next;
+
+    public Node(int data, Node pre, Node next) {
+        this.data = data;
+        this.pre = pre;
+        this.next = next;
+    }
 }
 }
