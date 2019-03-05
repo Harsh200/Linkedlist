a174d9447d674e9e1eaacbde4242ac6bd82282bb
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-05 15:05:28 +0530

    search method end

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -3,74 +3,77 @@ class Node {
     Node next;
     Node previous;
 }
-public class DoublyLinkedList{
-Node head;
-int size;
+public class DoublyLinkedList {
+    Node head;
+    int size;
 
     public static void main(String[] args) {
-DoublyLinkedList obj=new DoublyLinkedList();
+        DoublyLinkedList obj = new DoublyLinkedList();
         for (int i = 0; i < 10; i++) {
-            Node node=new Node();
-            node.data=(i+1)*100;
+            Node node = new Node();
+            node.data = (i + 1) * 100;
             obj.insert(node);
-            
+
         }
         obj.display();
         obj.delete();
     }
-    void insert (Node node){
-        System.out.println("inserting Node" + node.data +"atlast");
-        if(head==null){
-            head=node;
-        }
-        else{
-            Node temp=head;
-            while (temp.next!=null){
-            temp=temp.next;
+
+    void insert(Node node) {
+        System.out.println("inserting Node" + node.data + "atlast");
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            node.previous=temp;
-            temp.next=node;
+            node.previous = temp;
+            temp.next = node;
         }
     }//end of insert metho
 
-    void display(){
+    void display() {
         System.out.println("Doubly LinkedList");
-        Node temp=head;
+        Node temp = head;
         System.out.println("null <-->");
-        while (temp!=null){
-            System.out.println(temp.data +"<-->");
-            temp=temp.next;
+        while (temp != null) {
+            System.out.println(temp.data + "<-->");
+            temp = temp.next;
 
         }
         System.out.println("null");
     }
     //end of display method in doubly linked list
 
-    void delete(){
-    if(head==null){
-        System.out.println("empty list");
-    }
-    else {
-      Node temp=head;
-      while (temp.next!=null){
-          temp=temp.next;
-      }
-        System.out.println("deleting node" + temp.data +"from last");
-      temp=temp.previous;
-      temp.next=null;
-    }
-    boolean search(Node node){
-     boolean response=false;
-     Node temp=head;
-     while (temp!=null){
-         if(temp.data==node.data){
-             response = true;
-            break;
-         }
-               temp=temp.next;
-     }
+    void delete() {
+        if (head == null) {
+            System.out.println("empty list");
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            System.out.println("deleting node" + temp.data + "from last");
+            temp = temp.previous;
+            temp.next = null;
+        }
+        boolean search (Node node){
+            boolean response = false;
+            Node temp = head;
+            while (temp != null) {
+                if (temp.data == node.data) {
+                    response = true;
+                    break;
+                }
+                temp = temp.next;
+            }
+            return response;
+        }
+        void insertAtBegineer(Node node){
+
         }
     }
-
-
 }
+
+
