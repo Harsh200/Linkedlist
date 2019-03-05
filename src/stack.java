8981ff0d7a4bffec140ae2356ec86e578455378e
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-05 15:22:29 +0530

    making getter and setter method

--- src/stack.java
+++ src/stack.java
@@ -3,7 +3,24 @@ public class stack {
 private int data;
 private Node next;
 
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
         public Node(int data, Node next) {
+
             this.data = data;
             this.next = next;
         }
