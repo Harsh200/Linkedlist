f677757847a58d0cf7d2b70aaee1dcc13937b812
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-05 14:16:55 +0530

    calling insert method

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -12,6 +12,7 @@ DoublyLinkedList obj=new DoublyLinkedList();
         for (int i = 0; i < 10; i++) {
             Node node=new Node();
             node.data=(i+1)*100;
+            obj.insert(node);
             
         }
     }
