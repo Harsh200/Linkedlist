0047a39d47ba7637d3528c898a41620fba7e7b13
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-05 15:21:56 +0530

    creating construtor

--- src/stack.java
+++ src/stack.java
@@ -1,5 +1,11 @@
 public class stack {
     private static class Node{
+private int data;
+private Node next;
 
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
