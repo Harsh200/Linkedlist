952218291a78d840de05e3df980ca74713f6e25a
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-13 10:19:10 +0530

    creating constructor of node by only passing data

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -6,6 +6,10 @@ class Node{
     int data;
     Node next;
 
+    public Node(int data) {
+        this.data = data;
+    }
+
     public Node(int data, Node next) {
         this.data = data;
         this.next = next;
