df3275b8573825f768205a8574e76edc546e954f
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-03-13 10:57:01 +0530

    main method

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -1,47 +1,75 @@
 public class CircularLinkedList {
-int size=0;
-Node head=null;
-Node tail=null;
-class Node{
-    int data;
-    Node next;
+    int size = 0;
+    Node head = null;
+    Node tail = null;
 
-    public Node(int data) {
-        this.data = data;
+    public static void main(String[] args) {
+        CircularLinkedList linkedList = new CircularLinkedList();
+        linkedList.print();
+
     }
 
-    public Node(int data, Node next) {
-        this.data = data;
-        this.next = next;
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 
-    public int getData() {
-        return data;
+        public void print() {
+            if (size == 0) {
+                System.out.println("List is empty");
+            } else {
+                Node temp = head;
+                do {
+                    System.out.println(" " + temp.data);
+                    temp = temp.next;
+                }
+                while (temp != head);
+                System.out.println("");
+            }
+        }
+
+        public void addNodeToHead(int data) {
+            Node n = new Node(data);
+            if (size == 0) {
+                head = n;
+                tail = n;
+                n.next = head;
+
+            } else {
+                Node temp = head;
+                n.next = temp;
+                head = n;
+                tail.next = head;
+
+            }
+        }
     }
 
-    public void setData(int data) {
-        this.data = data;
-    }
 
-    public Node getNext() {
-        return next;
-    }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
-public void print(){
-Node temp=head;
-do{
-    System.out.println(" "+ temp.data);
-    temp=temp.next;
-}
-while(temp!=head);
-        System.out.println("");
-    }
-public void addNodeToHead(int data){
-        Node n=new Node(data);
 
-}
-}
-}
